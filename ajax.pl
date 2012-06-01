ce15ef346f653e71c9fc2343b5ea34229eec8018
balazs <balazs@bird.(none)>
2012-06-01 07:39:52 -0600

    clean up the main file

--- ajax.pl
+++ ajax.pl
@@ -5,55 +5,7 @@ use Stuff;
 
 get '/' => 'index';
 
-get '/ajax/:function/:param' => sub {
-    my $self = shift;
-
-    my $results = 'ERROR';
-
-    my $function = $self->param('function');
-
-    my $user = lc $self->param('param');
-    $user =~ s{_.*}{}; # sometimes the user name passed in has _$TIMESTAMP appended to it
-
-    my $users = Stuff::get_users();
-
-    if($function eq 'addnewuser'){
-	if(exists $users->{$user}){
-	    $results = "$user already exists";
-	}
-	else {
-	    $users->{$user} = {};
-	    Stuff::save_users($users);
-	    $results = 'OK';
-	}
-    }
-    elsif($function eq 'remove'){
-	if(exists $users->{$user}){
-	    delete $users->{$user};
-	    Stuff::save_users($users);
-	    $results = 'OK';
-	}
-	else {
-	    $results = "NO SUCH USER >$users<";
-	}
-    }
-    elsif($function =~ m{^(no|yes)favorite$}){
-	my $is_favorite = ($1 eq 'yes');
-	if(exists $users->{$user}){
-	    $users->{$user}{is_favorite} = $is_favorite;
-	    Stuff::save_users($users);
-	    $results = 'OK';
-	}
-	else {
-	    $results = "NO SUCH USER: >$user<";
-	}
-    }
-    else {
-        $results = "INVALID ACTION: >$function<";
-    }
-
-    $self->render_json({ result => $results});
-};
+get '/ajax/:function/:param' => sub { Stuff::ajaxhandler(shift) };
 
 app->start;
 
